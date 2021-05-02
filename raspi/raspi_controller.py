29b92ef1046e36b1b00cc365173314b4d0c2f8ac
Otieno Rowland <r.otieno@kokonetworks.com>
2021-05-03 00:46:31 +0300

    chore: use pin 12

--- raspi/raspi_controller.py
+++ raspi/raspi_controller.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 
 
 def should_drive_wheels(is_granted_access):
-    servo_pin = 17
+    servo_pin = 12
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(servo_pin, GPIO.OUT)
-    p = GPIO.PWM(servo_pin, 50)  # GPIO 17 for PWM with 50Hz
+    p = GPIO.PWM(servo_pin, 50)  # GPIO 12 for PWM with 50Hz
 
     if is_granted_access:
         p.start(2.5)  # Initialization
